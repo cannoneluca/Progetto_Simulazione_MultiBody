2dc3b30bb0f9b0c20dde366cca05ca9b2fc268a2
cannoneluca <luca.cannone@studenti.unipd.it>
2025-05-12 23:52:23 +0200

    creazione degli handler per l'animazione

--- render.m
+++ render.m
@@ -19,3 +19,17 @@ D.y = A.y + AD.z*sin(AD.f);
 E.x = D.x + DE.z*cos(DE.f);
 E.y = D.y + DE.z*sin(DE.f);
 
+%% Creazione degli handler per l'animazione
+h1 = line([A.x(1) O.x(1)], [A.y(1) O.y(1)], 'color', 'k', 'LineWidth', 2);
+h2 = line([O.x(1) C.x(1)], [O.y(1) C.y(1)], 'color', 'k', 'LineWidth', 2);
+h3 = line([C.x(1) B.x(1)], [C.y(1) B.y(1)], 'color', 'k', 'LineWidth', 2);
+h4 = line([B.x(1) A.x(1)], [B.y(1) A.y(1)], 'color', 'k', 'LineWidth', 2);
+h5 = line([A.x(1) D.x(1)], [A.y(1) D.y(1)], 'color', 'k', 'LineWidth', 2);
+h6 = line([D.x(1) E.x(1)], [D.y(1) E.y(1)], 'color', 'k', 'LineWidth', 2);
+h7 = line([E.x(1) F.x(1)], [E.y(1) F.y(1)], 'color', 'k', 'LineWidth', 2);
+h8 = line([F.x(1) G.x(1)], [F.y(1) G.y(1)], 'color', 'k', 'LineWidth', 2);
+h9 = line([G.x(1) C.x(1)], [G.y(1) C.y(1)], 'color', 'k', 'LineWidth', 2);
+
+
+
+
